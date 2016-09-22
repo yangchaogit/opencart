4850490e8fe35c4f6e0580e05f414ed08ad71c71
yangchaogit <yangchao@anychat.im>
2016-09-22 16:43:47 +0800

    add default chinese

---
+++ upload/admin/language/chinese/openbay/amazonus.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * $Author: http://www.opencartchina.com 
+**/
+// Heading
+$_['heading_title']         		= 'Amazon USA';
+$_['text_openbay']					= 'OpenBay Pro';
+$_['text_dashboard']				= 'Amazon US 仪表盘';
+
+// Text
+$_['text_heading_settings'] 		= 'Settings';
+$_['text_heading_account'] 			= 'Change plan';
+$_['text_heading_links'] 			= 'Item links';
+$_['text_heading_register'] 		= 'Register';
+$_['text_heading_bulk_listing'] 	= 'Bulk listing';
+$_['text_heading_stock_updates'] 	= 'Stock updates';
+$_['text_heading_saved_listings'] 	= 'Saved listings';
+$_['text_heading_bulk_linking'] 	= 'Bulk linking';
---
+++ upload/admin/language/chinese/openbay/ebay_usage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * $Author: http://www.opencartchina.com 
+**/
+// Headings
+$_['heading_title']             = 'Usage';
+$_['text_openbay']              = 'OpenBay Pro';
+$_['text_ebay']                 = 'eBay';
+
+// Text
+$_['text_usage']                = 'Your account usage';
+
+// Errors
+$_['error_ajax_load']      		= 'Sorry, could not get a response. Try later.';
